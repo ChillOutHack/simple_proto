7f6d386202951b1eddee7e50e2820ffa9f99487a
Matt Clement <darkmoves@gmail.com>
2014-04-26 10:39:53 -0400

    Create simple_pot sketch to use potentiometer

--- simple_pot/simple_pot.ino
+++ simple_pot/simple_pot.ino
@@ -1,6 +1,8 @@
 int peltier = 3; //The N-Channel MOSFET is on digital pin 3
 int power = 0; //Power level fro 0 to 99%
 int peltier_level = map(power, 0, 99, 0, 255); //This is a value from 0 to 255 that actually controls the MOSFET
+int const pot_pin = A0; //The pot input is Analog 0
+int pot_val; // This is the value of the pot, with a range of 0 - 1023
 
 void setup(){
 Serial.begin(9600);
@@ -11,24 +13,16 @@ Serial.begin(9600);
 void loop(){
 char option;
 
-if(Serial.available() > 0)
-{
-option = Serial.read();
-if(option == 'a') 
-power += 5;
-else if(option == 'z')
-power -= 5;
-
-if(power > 99) power = 99;
-if(power < 0) power = 0;
-
+pot_val = analogRead(pot_pin);
+power = map(pot_val, 0, 1023, 0, 99);
 peltier_level = map(power, 0, 99, 0, 255);
-}
 
 Serial.print("Power=");
 Serial.print(power);
 Serial.print(" PLevel=");
-Serial.println(peltier_level);
+Serial.print(peltier_level);
+Serial.print("pot_val=");
+Serial.println(pot_val);
 
 analogWrite(peltier, peltier_level); //Write this new value out to the port
 
