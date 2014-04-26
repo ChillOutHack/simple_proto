100079fbd4874c80c66a21f165b6154347f3a803
Matt Clement <darkmoves@gmail.com>
2014-04-26 17:04:02 -0400

    Add comments to lilypad temp and vibe sketch

--- lilypad_temp_and_vibe/lilypad_temp_and_vibe/lilypad_temp_and_vibe.ino
+++ lilypad_temp_and_vibe/lilypad_temp_and_vibe/lilypad_temp_and_vibe.ino
@@ -10,6 +10,7 @@ int average = 0;
 void setup(){
   Serial.begin(9600); //Start the serial connection in order to print outputs to the serial monitor
   pinMode(vibe_pin, OUTPUT); //Assign pin 11 as output
+  // This initializes the array for temperature smoothing
   for (int this_reading = 0; this_reading < num_readings; this_reading++)
     readings[this_reading] = 0;
 }
@@ -21,16 +22,17 @@ void loop(){
   float tempC = (voltage - 0.33) * 100; //Convert from 10mV per degree with 500 mV offset
   float tempF = (tempC * (9/5)) + 32; //Now convert to 'F
 
-  total = total - readings[index];
-  readings[index] = tempF;
-  total = total + readings[index];
-  index = index + 1;
+  total = total - readings[index]; //Subtracts the current index value from the total
+  readings[index] = tempF; //Assigns the current tempF reading to current index
+  total = total + readings[index]; //Adds current tempF reading to total
+  index = index + 1; //Increment the index by one
 
-  if (index >= num_readings)
+  if (index >= num_readings) //Resets index when it gets to highest value of the array
     index = 0;
 
-  average = total / num_readings;
+  average = total / num_readings; //Divides total temp by number of readings
 
+  //If loop to turn on motor above a certain temp
   if(average > 70.0){
     digitalWrite(vibe_pin, HIGH);
   }  
@@ -40,9 +42,9 @@ void loop(){
   
 
   // Print voltage to serial
-  Serial.print(voltage); Serial.print(" volts, ");
+  //Serial.print(voltage); Serial.print(" volts, ");
   // Print tempC to serial 
-  Serial.print(tempC); Serial.print(" degrees C, ");
+  //Serial.print(tempC); Serial.print(" degrees C, ");
   // Print tempF to serial
   Serial.print(tempF); Serial.println(" degrees F");
   // Print average tempF
