a894effd90dd6ec661a4627c4bb73a089758bb86
Vik <vik.paruchuri@gmail.com>
2014-04-26 13:28:34 -0400

    Add in temp sensor

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,6 +1,126 @@
 #include <SPI.h>
 #include <WiFi.h>
 
+// Wifi shield setup
+char ssid[] = "FBI Surveillance Van #42";     // the name of your network
+char pass[] = "44xzp44xzp";
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+byte postTo[] = {74, 125, 131, 104};
+WiFiClient postClient;
+WiFiServer server(80);
+
+// Pelier and potentiometer constants
+int peltier = 3; //The N-Channel MOSFET is on digital pin 3
+int power = 0; //Power level fro 0 to 99%
+int peltier_level = map(power, 0, 99, 0, 255); //This is a value from 0 to 255 that actually controls the MOSFET
+int const pot_pin = A0; //The pot input is Analog 0
+int pot_val; // This is the value of the pot, with a range of 0 - 1023
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  while (status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+  
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.print("Connected to the network");
+  printWifiStatus();
+  
+  server.begin();
+}
+
+void loop(){
+    // Code for pelier and potentiometer
+  char option;
+
+  pot_val = analogRead(pot_pin);
+  power = map(pot_val, 0, 1023, 0, 99);
+  peltier_level = map(power, 0, 99, 0, 255);
+  
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");       
+          }
+          client.print("Peltier level is ");
+          client.println(peltier_level);
+          client.println("</html>");
+           break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+  
+
+  
+  analogWrite(peltier, peltier_level); //Write this new value out to the port
+  
+
+}
+
+void printFirmwareVersion(){
+  Serial.print("Firmware version: ");
+  Serial.println(WiFi.firmwareVersion());
+}
+
+void postToServer(){
+  if (postClient.connect(postTo, 80)) {
+      Serial.println("connected");
+      // Make a HTTP request:
+      postClient.println("GET /search?q=arduino HTTP/1.1");
+      postClient.println("Host: www.google.com");
+      postClient.println("Connection: close");
+      postClient.println();
+  } else {
+    Serial.println("Could not get data from server");
+  }
+}
+
 void printMacAddress() {
   // the MAC address of your Wifi shield
   byte mac[6];                     
@@ -40,103 +160,6 @@ void listNetworks() {
     Serial.print(" dBm");
     Serial.print("\tEncryption: ");
     Serial.println(WiFi.encryptionType(thisNet));
-  }
-}
-
-char ssid[] = "FBI Surveillance Van #42";     // the name of your network
-char pass[] = "44xzp44xzp";
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
-byte postTo[] = {74, 125, 131, 104};
-WiFiClient postClient;
-WiFiServer server(80);
-
-void setup() {
-  // initialize serial:
-  Serial.begin(9600);
-  while (status != WL_CONNECTED) { 
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
-    status = WiFi.begin(ssid, pass);
-  
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-  Serial.print("Connected to the network");
-  printWifiStatus();
-  
-  server.begin();
-}
-
-void printFirmwareVersion(){
-  Serial.print("Firmware version: ");
-  Serial.println(WiFi.firmwareVersion());
-}
-
-void postToServer(){
-  if (postClient.connect(postTo, 80)) {
-      Serial.println("connected");
-      // Make a HTTP request:
-      postClient.println("GET /search?q=arduino HTTP/1.1");
-      postClient.println("Host: www.google.com");
-      postClient.println("Connection: close");
-      postClient.println();
-  } else {
-    Serial.println("Could not get data from server");
-  }
-}
-
-void loop(){
-    // listen for incoming clients
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");       
-          }
-          client.println("</html>");
-           break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    
-    // close the connection:
-    client.stop();
-    Serial.println("client disonnected");
   }
 }
 
